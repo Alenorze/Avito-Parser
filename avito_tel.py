2c295931937c2dd54e00502df1a5b4f84ee94629
Алексей Белов <Alenorze@gmail.com>
2018-02-12 00:27:09 +0200

    Img to String

--- avito_tel.py
+++ avito_tel.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from time import sleep
+from PIL import Image
+from pytesseract import image_to_string
+
 
 class Bot:
     def __init__(self):
@@ -9,9 +12,19 @@ class Bot:
     def take_screenshot(self):
         self.driver.save_screenshot('avito_screenshot.png')
     
-    # def crop(self):
-    #     image = 
-    
+    def tel_recon(self):
+        image = Image.open('tel.gif')
+        print(image_to_string(image))
+
+    def crop(self, location, size):
+        image = Image.open('avito_screenshot.png')
+        x = location['x']
+        y = location['y']
+        width = size['width']
+        height = size['height']
+        image.crop({x, y, x + width, y + height}).save('tel.gif')   
+        self.tel_recon()
+
     def navigate(self):
         self.driver.get('')
         button = self.driver.find_element_by_xpath('//button[@class="button item-phone-button js-item-phone-button button-origin button-origin-blue button-origin_small item-phone-button_header js-item-phone-button_header"]')
@@ -23,6 +36,7 @@ class Bot:
         location = image.location
         size = image.size 
         self.crop(location, size)
+
 
 def main():
     b = Bot()
