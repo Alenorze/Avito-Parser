348f9fef2545b1685e4a643d08f4865671d00a59
Алексей Белов <Alenorze@gmail.com>
2018-02-11 20:41:53 +0200

    Update Requirements.txt

--- avito.py
+++ avito.py
@@ -1,5 +1,7 @@
 import requests
-from bs4 import BeatifulSoup
+import csv
+
+from bs4 import BeautifulSoup
 
 
 def get_html(url):
@@ -7,17 +9,46 @@ def get_html(url):
     return r.text
 
 def get_total_pages(html):
-    soup = BeatifulSoup(html, 'lxml')
-    pages = soup.find('div', class_='pagintaion-class').find_all('a', class_="pagintaion-page")[-1].get('href')
+    soup = BeautifulSoup(html, 'lxml')
+    pages = soup.find('div', class_='pagination-class').find_all('a', class_="pagination-page")[-1].get('href')
     total_pages = pages.split('=')[1].split('&')[0]
     return int(total_pages)
 
+def write_csv(data):
+    with open('avito.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow( (data['title'], data['price'], data['metro'], data['url'] ))
+
 def get_page_data(html):
-    soup = BeatifulSoup(html, 'lxml')
+    soup = BeautifulSoup(html, 'lxml')
     ads = soup.find('div', class_='catalog_list').find_all('div', class_='item_table')
 
     for ad in ads:
-        
+        try:
+            title = ad.find('div', class_='description').find('h3').text.strip()
+        except:
+            title = ''
+        try:
+            url = 'https://www.avito.ru' + ad.find('div', class_='description').find('a').get('href')
+        except:
+            url = ''
+        try:
+            price = ad.find('div', class_='about').text.strip()
+        except:
+            price = ''
+        try:
+            metro = ad.find('div', class_='data').find_all('p')[-1].text.strip()
+        except:
+            metro = ''
+            
+        data = {'title': title,
+                'price': price,
+                'metro': metro,
+                'url': url}
+
+        write_csv(data)
+
+
 
 
 def main():
